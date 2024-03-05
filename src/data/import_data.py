22760fd65d422fe434995b28527001897ea68718
KamenDamov <kamendamov123@gmail.com>
2024-03-04 19:14:50 -0500

    cleaned repo and created a script to import data

--- src/data/import_data.py
+++ src/data/import_data.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+import datasets
+from datasets import load_dataset
+
+data = load_dataset("common_language")
+
 command = 'curl -X GET \
      "https://datasets-server.huggingface.co/rows?dataset=common_language&config=full&split=train&offset=0&length=100"'
 
