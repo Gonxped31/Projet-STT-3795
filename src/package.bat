18fadb46718ff329b6408e44604b97cf37c7616a
Simon Langlois <simonlanglois.sl@gmail.com>
2024-03-11 23:49:20 -0400

    pip imports.

---
+++ src/package.bat
@@ -0,0 +1,10 @@
+pip install scipy
+pip install mutagen
+pip install praat-parselmouth
+pip install matplotlib
+pip install pandas
+pip install librosa
+pip install noisereduce
+pip install jupyter
+pip install setuptools
+pip install torch
